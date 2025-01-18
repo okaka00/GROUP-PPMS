4a619529d61f469e2764f191e0df85c49810666c
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-18 09:19:22 +0800

    Update config.php

--- codes/user/config/config.php
+++ codes/user/config/config.php
@@ -1,1 +1,26 @@
+<?php
+/**
+ * using mysqli_connect for database connection
+ */
+$databaseHost = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = '';
+$databaseName = 'minicafe';
+$url = "http://localhost/campsite"; 
+define('BASE_URL', 'http://localhost/campsite/');
+define('BASE_PATH', __DIR__);
+define('ADMIN_BASE_URL', 'http://localhost/campsite/admin');
+
+
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+} 
+
+//echo "DB Connection Successful." . "<br>";
+?>
 
