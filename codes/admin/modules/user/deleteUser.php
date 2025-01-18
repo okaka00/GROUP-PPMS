e767a086d971d5744badd619137795d0566a6749
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-18 09:14:19 +0800

    Add files via upload

--- codes/admin/modules/user/deleteUser.php
+++ codes/admin/modules/user/deleteUser.php
@@ -1,1 +1,25 @@
+<?php
+//include db config
+include("../../config/config.php");
 
+if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['id'])) {
+    $userID = intval($_GET['id']);
+    // Delete the product record
+    $sql = "DELETE FROM user WHERE userID = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "User Id $userID deleted successfully!";
+        echo "<br><a href='" . ADMIN_BASE_URL . "'>Back to Admin Panel</a>";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+
+    mysqli_stmt_close($stmt);
+} else {
+    echo "Invalid request.";
+}
+
+mysqli_close($conn);
+?>
