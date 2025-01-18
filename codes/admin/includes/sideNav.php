8aec1692f735eb848088f6689fcbf872030d9cb1
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-18 09:13:22 +0800

    Update sideNav.php

--- codes/admin/includes/sideNav.php
+++ codes/admin/includes/sideNav.php
@@ -1,1 +1,44 @@
+<div class="sidenav">
+  <a href="/minicafe/admin">Dashboard</a>
+
+  <button class="dropdown-btn">User
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/user/userlist.php">Manage User</a>
+  </div>
+
+  <button class="dropdown-btn">Category
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/category/categoryForm.php">Create Category</a>
+        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/category/categoryList.php">Manage Category</a>
+  </div>
+
+  <button class="dropdown-btn">Product
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productForm.php">Create Product</a>
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productList.php">Manage Product</a>
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/orderList.php">Manage Order List</a>
+  </div>
+
+  <button class="dropdown-btn">Blog
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/blog/blogForm.php">Create Blog</a>
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/blog/blogList.php">Manage Blog</a>
+  </div>
+
+  <button class="dropdown-btn">Review
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Review</a>
+  </div>
+</div>
+
 
