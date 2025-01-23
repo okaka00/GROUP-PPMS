2309c06b3c2f31cdfae63ac517bf5a7e41f543c2
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-24 00:08:16 +0800

    Update login_action.php

--- codes/user/userAuth/login_action.php
+++ codes/user/userAuth/login_action.php
@@ -15,7 +15,7 @@ include("../config/config.php");
 <?php
 //login values from login form
 $userEmail = mysqli_real_escape_string($conn, $_POST['userEmail']);
-$userPwd = mysqli_real_escape_string($conn, $_POST['userPwd']);
+$userPwd = mysqli_real_escape_string($conn, $_POST['userPassword']);
 
 // Query to fetch the user details based on email
 $sql = "SELECT * FROM user WHERE userEmail='$userEmail' LIMIT 1";
@@ -25,38 +25,29 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 1) {	
 	$row = mysqli_fetch_assoc($result);
 	// Check if the password matches the hashed password in the database
-	if (password_verify($userPwd, $row['userPwd'])) {
+	if (password_verify($userPwd, $row['userPassword'])) {
   		// Successful login
 		$_SESSION["UID"] = $row["userID"];
 		$_SESSION["userName"] = $row["userName"];
-		$_SESSION["userRoles"] = $row["userRoles"];
+		$_SESSION["userRole"] = $row["roleID"];
 		$_SESSION['loggedin_time'] = time();  
 
 		// Redirect based on user role
-		if ($row['userRoles'] == 1) { // Admin
-			echo '<script type="text/javascript">
-				window.location.href = "' . ADMIN_BASE_URL . '/index.php";
-			</script>';
-		} else { // Normal User
-			echo '<script type="text/javascript">
-				window.location.href = "' . BASE_URL . '/index.php";
-			</script>';
-		}
+		if ($row['roleID'] == 1) { // Admin
+			header("Location: " . ADMIN_BASE_URL . "/index.php");	} 
+		else { // Normal User
+			header("Location: " . BASE_URL . "/index.php");		}
     	exit(); // Make sure to exit after redirection		
     } else {
 		// Incorrect password
-		echo '<script type="text/javascript">		
-			alert("Login error: Incorrect email or password.");
-		</script>';
-		echo '<a href="' . BASE_URL . 'index.php"> | Login |</a>';
-    }		
-} else {
+		echo 'Login error, user email and password is incorrect.<br>';//user email & pwd not correct	
+		header("Location: " . BASE_URL . "/index.php");                       
+	}		
+	} else {
 	// User does not exist
-	echo '<script type="text/javascript">
-		alert("Login error: User does not exist.");
-	</script>';
-	echo '<a href="' . BASE_URL . 'index.php"> | Login |</a>';
-} 
+	echo "Login error, user <b>$userEmail</b> does not exist. <br>";//user not exist
+	header("Location: " . BASE_URL . "/index.php");                
+	} 
 
 mysqli_close($conn);
 ?>
