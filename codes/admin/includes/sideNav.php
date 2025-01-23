6ffdfe1e7ede99feb403a479a12a1fe6435ab4ac
Afiqah Binti Ahmad Fairuze <official.afiqah02@gmail.com>
2025-01-23 15:05:09 +0800

    Update sideNav.php

--- codes/admin/includes/sideNav.php
+++ codes/admin/includes/sideNav.php
@@ -1,44 +1,68 @@
 <div class="sidenav">
-  <a href="/minicafe/admin">Dashboard</a>
+            <a href="/campsite/admin">Dashboard</a>
+          
+            <button class="dropdown-btn">User
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                  <a href="<?php echo ADMIN_BASE_URL; ?>/modules/user/userlist.php">Manage User</a>
+            </div>
+          
+            <!-- Campsite -->
+            <!-- AFIQAH BINTI AHMAD FAIRUZE (BI22110327) -->
+            <button class="dropdown-btn">Campsite
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                  <a href="<?php echo ADMIN_BASE_URL; ?>/modules/campsite/zoneForm.php">Create Zone</a>
+                  <a href="<?php echo ADMIN_BASE_URL; ?>/modules/campsite/zoneList.php">Manage Zone</a>
+                  <a href="<?php echo ADMIN_BASE_URL; ?>/modules/campsite/campsiteForm.php">Create Campsite</a>
+                  <a href="<?php echo ADMIN_BASE_URL; ?>/modules/campsite/campsiteList.php">Manage Campsite</a>
+            </div>
+          
+            <button class="dropdown-btn">Camp Tool
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+              <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productForm.php">Create Category</a>
+              <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productList.php">Create Tool</a>
+              <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/orderList.php">Manage Tool</a>
+            </div>
+          
+            <!-- Rental -->
+            <!-- AFIQAH BINTI AHMAD FAIRUZE (BI22110327) -->
+            <button class="dropdown-btn">Rental
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+              <a href="<?php echo ADMIN_BASE_URL; ?>/modules/rental/rentalList.php">Manage Rental</a>
+            </div>
+          
+            <button class="dropdown-btn">Review
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+              <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Review</a>
+            </div>
 
-  <button class="dropdown-btn">User
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/user/userlist.php">Manage User</a>
-  </div>
+          <button class="dropdown-btn">Blog
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-container">
+            <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Blog</a>
+          </div>
 
-  <button class="dropdown-btn">Category
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/category/categoryForm.php">Create Category</a>
-        <a href="<?php echo ADMIN_BASE_URL; ?>/modules/category/categoryList.php">Manage Category</a>
-  </div>
+          <button class="dropdown-btn">Memories
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-container">
+            <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Memories</a>
+          </div>
 
-  <button class="dropdown-btn">Product
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productForm.php">Create Product</a>
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/productList.php">Manage Product</a>
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/product/orderList.php">Manage Order List</a>
-  </div>
-
-  <button class="dropdown-btn">Blog
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/blog/blogForm.php">Create Blog</a>
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/blog/blogList.php">Manage Blog</a>
-  </div>
-
-  <button class="dropdown-btn">Review
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-    <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Review</a>
-  </div>
-</div>
-
-
+          <button class="dropdown-btn">Guidebook
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-container">
+            <a href="<?php echo ADMIN_BASE_URL; ?>/modules/review/reviewList.php">Manage Guidebook</a>
+          </div>
+        </div>
