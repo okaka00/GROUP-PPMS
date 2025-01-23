2bcacfe00b28b7ca8252ce25105743410d2d4ca8
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-23 20:58:00 +0800

    Create topNav.php

---
+++ codes/admin/includes/topNav.php
@@ -0,0 +1,19 @@
+<div class="topNav">
+        <img src="<?php echo ADMIN_BASE_URL; ?>/img/icon.png" alt="Logo">
+
+        <!-- if user in the session-->
+        <?php if (isset($_SESSION["UID"])) { ?>
+                <span>Welcome, <b><?php echo $_SESSION["userName"]; ?></b></span>
+                <a href="<?php echo BASE_URL; ?>/userAuth/logout.php" ><i class="fa fa-sign-in" style="font: size 12px;"></i> Logout</a>
+                
+        <?php } else {  ?>
+                        <!-- if no user in the session-->
+                <span>Welcome, <b><?php echo $_SESSION["userName"]; ?></b></span>
+                <a href="<?php echo BASE_URL; ?>" ><i class="fa fa-sign-in" style="font: size 12px;"></i>Login</a>
+                        
+        <?php }?>
+
+
+    </div>
+
+    
