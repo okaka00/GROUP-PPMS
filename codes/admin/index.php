2516ce5408883fe8884adf679f681b39e89a4cd6
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-23 20:57:23 +0800

    Update index.php

--- codes/admin/index.php
+++ codes/admin/index.php
@@ -8,31 +8,14 @@
         <link rel="stylesheet" type="text/css" href="<?php echo ADMIN_BASE_URL; ?>/css/admin.css">
     </head>
     <body>
-        <div class="topNav">
-            <img src="<?php echo ADMIN_BASE_URL; ?>/img/icon.png" alt="Logo">
-        </div>
         
         <?php
             include 'includes/sideNav.php'; 
         ?>
         
-        <!-- Login Popup -->
-		<div id="login-popup" class="login-popup">
-			<span class="close-btn" onclick="closeLoginPopup()">&times;</span>
-			<h3>Admin Login </h3>
-			<form action="userAuth/login_action.php" method="post">
-				<label for="userEmail">Admin Email:</label><br>
-				<input type="email" id="userEmail" name="userEmail" required><br><br>
-				<label for="userPwd">Password:</label><br>
-				<input type="password" id="userPwd" name="userPwd" required maxlength="8" autocomplete="off"><br><br>
-				<input type="submit" value="Login">
-				<input type="reset" value="Reset"></br>
-			</form>
-
-		</div>
-		<!-- Overlay -->
-		<div id="overlay" class="overlay" onclick="closeLoginPopup();"></div>
-		<!-- End Login Popup -->
+        <?php
+            include 'includes/topNav.php'; 
+        ?>
 
 
         <div class="main">
@@ -40,24 +23,6 @@
             <p>                 Welcome to the dashboard. Here you can manage various settings and options.</p>
         </div>
         
-        <script>
-            /* JS for dropdown content */
-            var dropdown = document.getElementsByClassName("dropdown-btn");
-            var i;
-            for (i = 0; i < dropdown.length; i++) {
-                dropdown[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
-                    var dropdownContent = this.nextElementSibling;
-                    if (dropdownContent.style.display === "block") {
-                        dropdownContent.style.display = "none";
-                    } else {
-                        dropdownContent.style.display = "block";
-                    }
-                });
-            }
-
-
-        </script>
+        <script src="includes/adminAuth.js"></script>
     </body>
 </html>
-
