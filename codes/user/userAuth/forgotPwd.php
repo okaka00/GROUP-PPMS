163d5a6fdf878c34053a4c2d183de9f1d39798b8
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-24 09:37:50 +0800

    Update forgotPwd.php

--- codes/user/userAuth/forgotPwd.php
+++ codes/user/userAuth/forgotPwd.php
@@ -3,17 +3,17 @@
 		<div id="fpwd-popup" class="fpwd-popup">
 			<span class="close-btn" onclick="closeFpwdPopup()">&times;</span>
 			<h3>User Forgot Password </h3>
-			<form action="userAuth/login_action.php" method="post">
+			<form action="userAuth/forgotPwd_action.php" method="post">
 				<label for="userEmail">User Email:</label><br>
-				<input type="email" id="userEmail" name="userEmail" required><br><br>
+				<input type="email" class="test" id="userEmail" name="userEmail" required><br><br>
 				<label for="userPassword">New Password:</label><br>
 				<input type="password" id="userPassword" name="userPassword" required maxlength="8" autocomplete="off"><br><br>
-                <label for="checkNewPwd">Confirm Password:</label>
+                <label for="checkNewPwd">Confirm Password:</label><br>
                 <input type="password" placeholder="ReEnter New Password" id="checkNewPwd" name="confirmNewPwd"required>
 				<button type="submit" class="submit-btn" >CONFIRM</button>
 				<button type="reset" class="reset-btn" value="Reset">RESET</button>
 			</form>
-			<p><a href="javascript:void(0);" onclick="closeFpwdPopup();">| Back to Home Page | </a></p>
+			<p><a href="index.php">| Back to Home Page | </a></p>
 		</div>
 		<!-- Overlay -->
 		<div id="overlay" class="overlay" onclick="closeFpwdPopup();"></div>
