30bdde23f508816b286dad21bf2deaf695c2b593
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-24 08:00:25 +0800

    Update topnav.php

--- codes/user/includes/topnav.php
+++ codes/user/includes/topnav.php
@@ -2,20 +2,21 @@
 
     <a href="index.php">Home</a>
     <a href="photo_album.php">Photo Album</a>
-    <a href="guidebook.php">Guidebook</a>
     <a href="promotions_events.php">Promotions & Events</a>
+    <a href="campsites.php">Campsites</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i></a>
         
         
         <?php if (isset($_SESSION["UID"])) { ?>
             
-    <a href="campsites.php">Campsites</a>
-    <a href="tools_rental.php">Tools Rental</a>
-    <a href="bookings.php">Bookings</a>
-    <a href="blog.php">Blog</a>
-    <a href="review.php">Reviews</a>
+            <a href="tools_rental.php">Tools Rental</a>
+            <a href="bookings.php">Bookings</a>
+            <a href="guidebook.php">Guidebook</a>
+            <a href="blog.php">Blog</a>
+            <a href="review.php">Reviews</a>
     <?php }?>
 
     
 </nav>
+
