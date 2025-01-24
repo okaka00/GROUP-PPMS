f2de6032721e76ffa6cb65ac9a1c1a3769e37318
MaslindaSulaiman <145274598+okaka00@users.noreply.github.com>
2025-01-24 11:16:16 +0800

    Update register.php

--- codes/user/userAuth/register.php
+++ codes/user/userAuth/register.php
@@ -4,13 +4,13 @@
 			<h3>User Registration </h3>
 			<form action="userAuth/register_action.php" method="post">
 				<label for="reguserName">Username:</label><br>
-				<input type="text" id="reguserName" name="userName" required><br><br>
+				<input type="text" class="test" id="reguserName" name="userName"required><br><br>
 				<label for="reguserEmail">User Email:</label><br>
-				<input type="email" id="reguserEmail" name="userEmail" required><br><br>
+				<input type="email" class="test" id="reguserEmail" name="userEmail" required><br><br>
 				<label for="reguserPwd">Password:</label><br>
-				<input type="password" id="reguserPwd" name="userPwd" required maxlength="8"><br><br>
+				<input type="password" class="test" id="reguserPwd" name="userPwd" required maxlength="8"><br><br>
 				<label for="regconfirmPwd">Confirm Password:</label><br>
-				<input type="password" id="regconfirmPwd" name="confirmPwd" required><br><br>
+				<input type="password" class="test" id="regconfirmPwd" name="confirmPwd" required><br><br>
 				<button type="submit" class="submit-btn" value="Register" >REGISTER</button>
 				<button type="reset" class="reset-btn" value="Reset">RESET</button>
 			</form>
